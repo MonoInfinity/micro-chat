43d7769fc7ad39631e70512958f54721dd0453eb
heaty566 <heaty566@gmail.com>
2021-12-04 02:11:15 +0700

    ADD logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { monoLogger } from 'mono-utils-core';
+import { router } from './router';
+
+export const NS_APP = 'app-main';
 
 async function bootstrap() {
       const app = await NestFactory.create(AppModule);
-      await app.listen(3000);
+      router(app);
+      const port = process.env.PORT || 4000;
+
+      await app.listen(port, () => {
+            monoLogger.log(NS_APP, `---------------Configuration--------------------`);
+            monoLogger.printEnv(NS_APP, {
+                  //app
+                  PORT: process.env.PORT || '',
+                  NODE_ENV: process.env.NODE_ENV || '',
+                  DEBUG: process.env.DEBUG || '',
+            });
+            monoLogger.log(NS_APP, `-----------------------------------`);
+            monoLogger.log(NS_APP, `Current Mode: ${process.env.NODE_ENV}`);
+            monoLogger.log(NS_APP, `Listening on port ${port}`);
+            monoLogger.log(NS_APP, `Ready to service`);
+      });
 }
 bootstrap();
