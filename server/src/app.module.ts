1fefcc55570aeed46763e047c8be6c46c731f38c
heaty566 <heaty566@gmail.com>
2021-12-04 01:52:18 +0700

    [MC-0002] add config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+const Config = ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `config/.env`,
+});
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+      imports: [Config],
+      controllers: [AppController],
+      providers: [AppService],
 })
 export class AppModule {}
